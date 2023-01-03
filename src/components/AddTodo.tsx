f05de27c677defb69418b894fe25e099ae10c12b
WaruiAlfred <alfredwarui17@gmail.com>
2023-01-03 16:54:01 +0300

    add content to AddTodo component

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,7 +1,28 @@
-import * as React from "react";
+import { useContext } from "react";
+import { TodoContextType } from "../@types/todo";
+import { TodoContext } from "../context/store";
 
 export interface IAddTodoProps {}
 
 export default function AddTodo(props: IAddTodoProps) {
-  return <div></div>;
+  const { newTask, setNewTask, addTask } = useContext(
+    TodoContext
+  ) as TodoContextType;
+
+  return (
+    <div className="row mb-20">
+      <div className="col">
+        <input
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+          className="form-control form-control-lg"
+        />
+      </div>
+      <div className="col-auto">
+        <button onClick={addTask} className="btn btn-lg btn-success">
+          Add Todo
+        </button>
+      </div>
+    </div>
+  );
 }
