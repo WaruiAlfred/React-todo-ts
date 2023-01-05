79b8e4a24b2c39163d9e563dad84c208ac8d7b1e
WaruiAlfred <alfredwarui17@gmail.com>
2023-01-05 20:28:55 +0300

    add Todos component data

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,7 +1,64 @@
-import * as React from "react";
+import { Fragment, useContext } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCircleCheck,
+  faPen,
+  faTrashCan,
+} from "@fortawesome/free-solid-svg-icons";
+import { TodoContext } from "../context/store";
+import { ITodo, TodoContextType } from "../@types/todo";
 
-export interface ITodosProps {}
+export interface ITodosProps {
+  setUpdateData: (data: ITodo) => void;
+}
 
 export default function Todos(props: ITodosProps) {
-  return <div></div>;
+  const { todos, markTaskDone, deleteTask } = useContext(
+    TodoContext
+  ) as TodoContextType;
+
+  return (
+    <Fragment>
+      {todos && todos.length ? "" : "No todos added!"}
+
+      {todos &&
+        todos
+          .sort((a, b) => (b.id > a.id ? 1 : -1))
+          .map((task, index) => (
+            <Fragment key={task.id}>
+              <div className="col taskBg">
+                <div className={task.status ? "done" : ""}>
+                  <span className="taskNumber">{todos.length - index}</span>
+                  <span className="taskText">{task.title}</span>
+                </div>
+                <div className={` iconsWrap ${task.status ? "undo" : ""}`}>
+                  <span
+                    onClick={() => markTaskDone(task.id)}
+                    title="Completed / Not Completed"
+                  >
+                    <FontAwesomeIcon icon={faCircleCheck} />
+                  </span>
+                  {!task.status && (
+                    <span
+                      onClick={() =>
+                        props.setUpdateData({
+                          id: task.id,
+                          title: task.title,
+                          status: task.status ? true : false,
+                        })
+                      }
+                      title="Edit"
+                    >
+                      <FontAwesomeIcon icon={faPen} />
+                    </span>
+                  )}
+                  <span onClick={() => deleteTask(task.id)} title="Delete">
+                    <FontAwesomeIcon icon={faTrashCan} />
+                  </span>
+                </div>
+              </div>
+            </Fragment>
+          ))}
+    </Fragment>
+  );
 }
