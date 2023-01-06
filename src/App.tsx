1b331d8e222c95d96e06d94785f34034347de64d
WaruiAlfred <alfredwarui17@gmail.com>
2023-01-06 17:03:00 +0300

    adjust app.tsx content

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import UpdateTodo from "./components/UpdateTodo";
+import AddTodo from "./components/AddTodo";
+import Todos from "./components/Todos";
+import TodoContextProvider from "./context/store";
+import { useState } from "react";
+import { ITodo } from "./@types/todo";
 
 function App() {
+  const [updateData, setUpdateData] = useState<ITodo | any>("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <TodoContextProvider>
+      <div className="container App">
+        <Header />
+
+        {updateData && updateData ? (
+          <UpdateTodo updateData={updateData} setUpdateData={setUpdateData} />
+        ) : (
+          <AddTodo />
+        )}
+
+        <Todos setUpdateData={setUpdateData} />
+      </div>
+    </TodoContextProvider>
   );
 }
 
